4d059f8cac28a0056b032018518ecdc2bba3a77c
Adam Lister <admlw@users.noreply.github.com>
2022-08-25 16:45:51 -0500

    modifying the MPLCONFIGDIR inside the __init__.py doesn't update the actual directory that mpl is searching for styles, so instead just directly use that envar to set the style directly

--- src/matplotlib/python/__init__.py
+++ src/matplotlib/python/__init__.py
@@ -1,1 +1,11 @@
+import os
+import matplotlib
+
+mplconfigpath = os.path.dirname(os.path.realpath(__file__))+"/style/"
+
+if 'MPLCONFIGDIR' in os.environ:
+    os.environ['MPLCONFIGDIR'] = mplconfigpath + os.pathsep + os.environ['MPLCONFIGDIR']
+else:
+    os.environ['MPLCONFIGDIR'] = mplconfigpath
+
 from .dunestyle import *

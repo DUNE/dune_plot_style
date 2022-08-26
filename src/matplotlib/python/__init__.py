5583e6a11f6934ed3f8596e8630b5e23209bed0d
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2022-08-26 12:21:20 -0400

    install the stylelib and set it up for import

--- src/matplotlib/python/__init__.py
+++ src/matplotlib/python/__init__.py
@@ -1,1 +1,8 @@
+try:
+    import os, os.path, pathlib
+    import dunestyle.stylelib as module
+    os.environ["MPLCONFIGDIR"] = str(pathlib.Path(module.__path__[0]).parent.absolute()) + (":" + os.environ["MPLCONFIGDIR"] if "MPLCONFIGDIR" in os.environ else "")
+except:
+    pass
+
 from .dunestyle import *

a5c65dd02894c41677e971b920ff4151f8608f15
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2022-08-26 13:42:11 -0400

    add a comment explaining why this black magic is necessary

--- src/matplotlib/python/__init__.py
+++ src/matplotlib/python/__init__.py
@@ -1,4 +1,7 @@
 try:
+    # this handles the arrangement that comes out
+    #  when dune-plot-style is set up standalone by using `pip install`.
+    # (the UPS product sets $MPLCONFIGDIR directly in its setup process.)
     import os, os.path, pathlib
     import dunestyle.stylelib as module
     os.environ["MPLCONFIGDIR"] = str(pathlib.Path(module.__path__[0]).parent.absolute()) + (":" + os.environ["MPLCONFIGDIR"] if "MPLCONFIGDIR" in os.environ else "")

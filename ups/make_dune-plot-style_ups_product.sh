a8ca2556055e001f9378f0fc9cd658fdad8564f3
Adam <alister1@FNAL.GOV>
2022-08-26 14:38:50 -0500

    uncomment a bunch of junk, don't print out tree any more

--- ups/make_dune-plot-style_ups_product.sh
+++ ups/make_dune-plot-style_ups_product.sh
@@ -67,71 +67,69 @@ mv ${tmpdir}/${reponame}-preorg/src/__init__.py          ${tmpdir}/${reponame}/p
 mv ${tmpdir}/${reponame}-preorg/src/matplotlib/python/*  ${tmpdir}/${reponame}/python/dunestyle/matplotlib
 mv ${tmpdir}/${reponame}-preorg/src/root/python/*        ${tmpdir}/${reponame}/python/dunestyle/root
 
-tree ${tmpdir}/${reponame}
+proddir=${path}/${reponame}
+dest=${proddir}/${version}/NULL
 
-#proddir=${path}/${reponame}
-#dest=${proddir}/${version}/NULL
-#
-#echo "$reponame will be created in $dest"
-#
-#if [ ! -d "${proddir}" ]; then
-#  mkdir -p ${proddir}
-#fi
-#
-## offer option to overwrite a product if it exists, though
-## in general we won't want to do this
-#if [ -d "${proddir}/${version}" ]; then
-#  echo ""
-#  echo "Product ${reponame} with version ${version} already exists." 
-#  echo "Making it again will over-write the existing one."
-#  echo ""
-#  read -p "Are you sure you want to proceed (y/n)? " -n 1 -r
-#  echo   
-#  if [[ $REPLY =~ ^[Yy]$ ]]
-#  then
-#    echo "OK. If you say so."
-#  else
-#    echo "The script will now abort. Try again with a different version."
-#    exit -1
-#  fi
-#fi
-#
-## now copy the code to it's location in the /grid area
-#mkdir -p ${dest}
-#rsync --exclude '*~' --exclude '*.git' -rL $tmpdir/${reponame}/* ${dest}
-#
-## update the ups table to give the correct version number
-#ups_table=${dest}/ups/${reponame}.table
-#if [ ! -f "${ups_table}" ] ; then
-#  echo ""
-#  echo "Error! UPS table ${ups_table} does not exist!"
-#  echo ""
-#  exit -1
-#fi
-#
-#echo "Updating table file"
-#
-#sed -i -e "s:XXVERSIONXX:${version}:" \
-#  ${ups_table}
-#
-#echo"Declaring product ${reponame} with version ${version} to UPS."
-#
-## declare to ups
-#ups declare -f NULL -z ${path} \
-#  -r ${path}/${reponame}/${version}/NULL \
-#  -m ${reponame}.table \
-#  ${reponame} ${version}
-#
-#retval=$?
-#test $retval -ne 0 && echo "Error! 'ups declare' returned non-zero - BAILING" && exit 1
-#
-## add to upd
-#cd ${proddir}/${version}/NULL/
-#
-#upd addproduct ${reponame} ${version} 
-#retval=$?
-#test $retval -ne 0 && echo "Error! 'upd addproduct' returned non-zero - BAILING" && exit 1
-#
-#rm -rf ${tmpdir}
-#
-#echo "Done"
+echo "$reponame will be created in $dest"
+
+if [ ! -d "${proddir}" ]; then
+  mkdir -p ${proddir}
+fi
+
+# offer option to overwrite a product if it exists, though
+# in general we won't want to do this
+if [ -d "${proddir}/${version}" ]; then
+  echo ""
+  echo "Product ${reponame} with version ${version} already exists." 
+  echo "Making it again will over-write the existing one."
+  echo ""
+  read -p "Are you sure you want to proceed (y/n)? " -n 1 -r
+  echo   
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    echo "OK. If you say so."
+  else
+    echo "The script will now abort. Try again with a different version."
+    exit -1
+  fi
+fi
+
+# now copy the code to it's location in the /grid area
+mkdir -p ${dest}
+rsync --exclude '*~' --exclude '*.git' -rL $tmpdir/${reponame}/* ${dest}
+
+# update the ups table to give the correct version number
+ups_table=${dest}/ups/${reponame}.table
+if [ ! -f "${ups_table}" ] ; then
+  echo ""
+  echo "Error! UPS table ${ups_table} does not exist!"
+  echo ""
+  exit -1
+fi
+
+echo "Updating table file"
+
+sed -i -e "s:XXVERSIONXX:${version}:" \
+  ${ups_table}
+
+echo"Declaring product ${reponame} with version ${version} to UPS."
+
+# declare to ups
+ups declare -f NULL -z ${path} \
+  -r ${path}/${reponame}/${version}/NULL \
+  -m ${reponame}.table \
+  ${reponame} ${version}
+
+retval=$?
+test $retval -ne 0 && echo "Error! 'ups declare' returned non-zero - BAILING" && exit 1
+
+# add to upd
+cd ${proddir}/${version}/NULL/
+
+upd addproduct ${reponame} ${version} 
+retval=$?
+test $retval -ne 0 && echo "Error! 'upd addproduct' returned non-zero - BAILING" && exit 1
+
+rm -rf ${tmpdir}
+
+echo "Done"

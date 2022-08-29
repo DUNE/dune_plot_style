59c0689f5db6c0130db45b5f7310d2754e222a3c
Andrew Mogan <andrew.mogan@colostate.edu>
2022-08-29 08:47:17 -0700

    Add 1D histogram to matplotlib examples

--- examples/matplotlib/example.py
+++ examples/matplotlib/example.py
@@ -8,6 +8,21 @@ from matplotlib import pyplot as plt
 
 import dunestyle as dunestyle
 
+# 1D Gaussian example
+mu, sigma = 0, 1
+nbins = 50
+x_gaus = np.random.normal(mu, sigma, 1000)
+
+#plt.hist(x_gaus, nbins, label="Hist", histtype='step')
+plt.hist(x_gaus, nbins, label="Hist", histtype='step')
+plt.xlabel('x label')
+plt.ylabel('y label')
+plt.xlim([-5,5])
+plt.legend()
+dunestyle.WIP()
+dunestyle.SimulationSide()
+plt.savefig("example.1Dhist.matplotlib.png")
+
 x = np.linspace(-5, 5, 500)
 y = scipy.stats.norm.pdf(x)
 

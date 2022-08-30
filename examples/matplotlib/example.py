0f5f739d21b6b20d92cc8dac28411cab245057e7
Andrew Mogan <amogan@vols.utk.edu>
2022-08-30 11:12:06 -0500

    Add Gauss fit data/MC example

--- examples/matplotlib/example.py
+++ examples/matplotlib/example.py
@@ -4,24 +4,91 @@ matplotlib placeholder example.  we can do way better than this.
 
 import numpy as np
 import scipy.stats
+from scipy.optimize import curve_fit
 from matplotlib import pyplot as plt
+import matplotlib.gridspec as gridspec
 
-import dunestyle as dunestyle
+import dunestyle.matplotlib as dunestyle
 
-# 1D Gaussian example
+# The first two example plots will use the same Gaussian distribution.
+# Here, we save the Gaussian as a numpy histogram, but this isn't 
+# strictly necessary. It just makes data manipulation easier and 
+# allows us to manipulate the histogram data without drawing it
+# (relevant in the data/MC plot, where we want to convert the
+# histogram to points).
 mu, sigma = 0, 1
 nbins = 50
 x_gaus = np.random.normal(mu, sigma, 1000)
 
-#plt.hist(x_gaus, nbins, label="Hist", histtype='step')
-plt.hist(x_gaus, nbins, label="Hist", histtype='step')
+hist, bin_edges = np.histogram(x_gaus, bins=50, range=(-5,5))
+bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
+# Dummy error values; just for illlustration
+y_error = np.std(x_gaus)/np.sqrt(x_gaus.size)*hist
+
+# 1D Gaussian example
+# The "stairs" plot is the easiest way to plot a histogram
+# from a numpy histogram. If you're not using a numpy histogram,
+# then use plt.hist().
+plt.stairs(hist, bin_edges, fill=False, label="Hist", linewidth=1.5)
 plt.xlabel('x label')
 plt.ylabel('y label')
-plt.xlim([-5,5])
 plt.legend()
 dunestyle.WIP()
 dunestyle.SimulationSide()
 plt.savefig("example.1Dhist.matplotlib.png")
+
+# Data/MC example
+# For this example, we take our "data" from the above 1D Gaussian histogram
+
+# Gaus fits are not as straightforward in matplotlib as they are
+# in ROOT. See the second example at
+# https://physics.nyu.edu/pine/pymanual/html/chap8/chap8_fitting.html
+
+def Gauss(x, H, A, x0, sigma):
+  return H + A * np.exp(-(x - x0) ** 2 / (2 * sigma ** 2))
+
+# Use SciPy's curve_fit function to return optimal fit
+# parameters (popt) and the covariance matrix (pconv)
+# Note that you need to give curve_fit a good initial guess
+# in order for it to converge (see p0 below)
+def Gauss_fit(x, y):
+  mean = sum(x * y) / sum(y)
+  sigma = np.sqrt(sum(y * (x - mean) ** 2) / sum(y))
+  popt, pcov = curve_fit(Gauss, x, y, p0=[min(y), max(y), mean, sigma])
+  return popt, pcov
+
+fig = plt.figure(figsize=(10,6))
+gs = gridspec.GridSpec(nrows=2, ncols=1, height_ratios=[3, 1])
+
+popt, pcov = Gauss_fit(bin_centers, hist)
+
+# Unpack fit parameters from popt and uncertainties from 
+# diagonal of covariance matrix
+H, A, x0, sig = popt
+dH, dA, dx0, dsig = [np.sqrt(pcov[j,j]) for j in range(popt.size)]
+
+# Create fitting function
+x_fit = bin_centers
+y_fit = Gauss(x_fit, H, A, x0, sig)
+
+residuals = hist/Gauss(bin_centers, H, A, x0, sigma)
+
+# Top plot
+ax0 = fig.add_subplot(gs[0, 0])
+ax0.set_ylabel("y label")
+ax0.plot(x_fit, y_fit, color='r', label="Fit")
+ax0.errorbar(x=bin_centers, y=hist,
+             yerr=y_error, fmt='o', capsize=0.5, label="Data")
+ax0.legend()
+
+# Bottom plot
+ax1 = fig.add_subplot(gs[1, 0], sharex=ax0)
+ax1.errorbar(x=bin_centers, y=residuals,
+             yerr=y_error, fmt='o', capsize=1, label="Ratio")
+ax1.axhline(color="r", zorder=-1)
+ax1.set_xlabel("x label")
+ax1.set_ylabel("Residuals")
+plt.savefig("example.datamc.matplotlib.png")
 
 x = np.linspace(-5, 5, 500)
 y = scipy.stats.norm.pdf(x)
